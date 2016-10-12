22cafc6744e66aa2a08520f5c0a2eb6c000d744e
gc_chenqiu <gc_chenqiu@huoyunren.com>
2016-10-12 10:42:00 +0800

    add pdf file op.
    Signed-off-by: gc_chenqiu <gc_chenqiu@huoyunren.com>

--- Sync2kindle/src/main/java/com/chinaway/kindle/mail/Snippet.java
+++ Sync2kindle/src/main/java/com/chinaway/kindle/mail/Snippet.java
@@ -19,20 +19,20 @@ import java.util.Date;
 public class Snippet {
     public static void main(String[] args){   
              //这个类主要是设置邮件   
-//          MailSenderInfo mailInfo = new MailSenderInfo();    
-//          mailInfo.setMailServerHost("smtp.163.com");    
-//          mailInfo.setMailServerPort("25");    
-//          mailInfo.setValidate(true);    
-//          mailInfo.setUserName("zaqw396056737");    
-//          mailInfo.setPassword("chenqiu ");//您的邮箱密码    
-//          mailInfo.setFromAddress("zaqw396056737@163.com");    
-//          mailInfo.setToAddress("wpty@kindle.cn");    
-//          mailInfo.setSubject("设置邮箱标题 如http://www.guihua.org 中国桂花网");    
-//          mailInfo.setContent("设置邮箱内容 如http://www.guihua.org 中国桂花网 是中国最大桂花网站==");    
-//             //这个类主要来发送邮件   
-//          SimpleMailSender sms = new SimpleMailSender();   
-//              sms.sendTextMail(mailInfo);//发送文体格式    
-//              //sms.sendHtmlMail(mailInfo);//发送html格式   
+          MailSenderInfo mailInfo = new MailSenderInfo();    
+          mailInfo.setMailServerHost("smtp.163.com");    
+          mailInfo.setMailServerPort("25");    
+          mailInfo.setValidate(true);    
+          mailInfo.setUserName("zaqw396056737");    
+          mailInfo.setPassword("chenqiu ");//您的邮箱密码    
+          mailInfo.setFromAddress("zaqw396056737@163.com");    
+          mailInfo.setToAddress("wpty@kindle.cn");    
+          mailInfo.setSubject("设置邮箱标题 如http://www.guihua.org 中国桂花网");    
+          mailInfo.setContent("设置邮箱内容 如http://www.guihua.org 中国桂花网 是中国最大桂花网站==");    
+             //这个类主要来发送邮件   
+          SimpleMailSender sms = new SimpleMailSender();   
+              sms.sendTextMail(mailInfo);//发送文体格式    
+              //sms.sendHtmlMail(mailInfo);//发送html格式   
         }  
 }  
 
